df8df912aa171c45e4a9d81823f4f5edbebce540
unknown <nikitamane2000@gmail.com>
2025-05-31 09:40:51 +0530

    first commit

---
+++ src/main/java/com/example/testKubernet/TestKubernetApplication.java
@@ -0,0 +1,13 @@
+package com.example.testKubernet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestKubernetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestKubernetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testKubernet/TestKubernetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testKubernet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestKubernetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
